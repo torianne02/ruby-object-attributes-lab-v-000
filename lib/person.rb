9c8283ed972316b6ceedde92cc9fcf86ab59aa0f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-24 18:58:25 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,24 +2,4 @@ class Person
   def initialize(name)
     @name = name
   end
-
-  def name
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def initialize(job)
-    @job = job
-  end
-
-  def job
-    @job
-  end
-
-  def job=(job)
-    @job = job
-  end
 end
